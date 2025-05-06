152623b22b0a4bfc006b1a76be17d1f1b809c47c
Kauana-bm24 <kauana.bm24@aluno.ifsc.edu.br>
2025-05-06 00:30:02 +0000

    pessoa e atributos

---
+++ pessoa.java
@@ -0,0 +1,27 @@
+public class Pessoa { no usages
+    private String nome ; 2 usages
+    //Atributos
+    public int getIdade() { no usages
+        return idade;
+
+ // Contrutores
+    }
+ public Pessoa(int idade) { mno usage
+this.idade = idade
+}
+    
+    //geteers
+    public void setNome( String Nome) {   no usages
+        if (nome.isEmpty()) {
+            this.nome = nome;
+
+        }
+    }
+    public void setIdade (int idade) { no usages
+        this.idade = idade;
+ 
+    }
+    public void felizAniversario(
+        this.++;
+    )
+}
